28d932048a2db5dad4220d440a457a4b4c175833
FinByz Tech Pvt. Ltd <info@finbyz.com>
2019-06-18 18:45:46 +0530

    Initial commit

---
+++ hooks.py
@@ -0,0 +1,12 @@
+# Include below code in your hooks.py
+
+app_include_js = [
+	"assets/js/summernote.min.js",
+	"assets/js/comment_desk.min.js",
+	"assets/js/editor.min.js",
+	"assets/js/timeline.min.js"
+]
+
+app_include_css = [
+	"assets/css/summernote.min.css"
+]
